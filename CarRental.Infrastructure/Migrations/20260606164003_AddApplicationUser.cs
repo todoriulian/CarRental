cbcd7f37c684fee02493ae1876d4daa28cf1d7d7
Iulian Todor - Tenaris <itodor@tenaris.com>
2026-06-06 19:42:35 +0300

    add migration

---
+++ CarRental.Infrastructure/Migrations/20260606164003_AddApplicationUser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarRental.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApplicationUsers",
+                columns: table => new
+                {
+                    Guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    GoogleSubjectId = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    Deleted = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationUsers", x => x.Guid);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationUsers_GoogleSubjectId",
+                table: "ApplicationUsers",
+                column: "GoogleSubjectId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationUsers");
+        }
+    }
+}
